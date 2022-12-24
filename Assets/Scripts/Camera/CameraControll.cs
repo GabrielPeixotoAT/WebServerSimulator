3061c821c19a993bb86bf0ddafc752dab0ae6c79
Gabriel Peixoto <gabr.art.design@gmail.com>
2022-12-24 15:19:43 -0300

    feat: main menu

--- Assets/Scripts/Camera/CameraControll.cs
+++ Assets/Scripts/Camera/CameraControll.cs
@@ -3,32 +3,59 @@ using UnityEngine.SceneManagement;
 
 public class CameraControll : MonoBehaviour
 {
-    public GameObject menu;
+    public ServerStatus ServerStatusMenu;
+
+    Transform oldHit;
+    bool lastIsServer;
 
     void Update()
     {
         transform.Translate(new Vector3(Input.GetAxis("Mouse X") / 1000, Input.GetAxis("Mouse Y") / 1000, 0));
 
-        if (Input.GetMouseButtonDown(0))
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            //Select stage    
+            if (hit.transform.tag == "server")
             {
-                //Select stage    
-                if (hit.transform.tag == "server")
+                hit.transform.GetComponent<Outline>().OutlineWidth = 2;
+
+                if (oldHit != null)
                 {
-                    menu.SetActive(true);
+                    if (oldHit.name != hit.transform.name)
+                    {
+                        oldHit.GetComponent<Outline>().OutlineWidth = 0;
+                    }
                 }
-                else
+                
+                lastIsServer = true;
+                oldHit = hit.transform;
+
+                if (Input.GetMouseButtonDown(0))
                 {
-                    menu.SetActive(false);
+                    ServerStatusMenu.gameObject.SetActive(true);
+                    ServerStatusMenu.server = hit.transform.gameObject.GetComponent<Server>();
                 }
             }
             else
             {
-                menu.SetActive(false);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    ServerStatusMenu.gameObject.SetActive(false);
+                }
+
+                if (lastIsServer)
+                {
+                    oldHit.transform.GetComponent<Outline>().OutlineWidth = 0;
+                    lastIsServer = false;
+                }
             }
+        }
+        else
+        {
+            ServerStatusMenu.gameObject.SetActive(false);
         }
     }
 }
